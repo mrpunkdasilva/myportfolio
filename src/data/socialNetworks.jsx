95f29bf4db7c5efd77497e0a089b895f72352870
Mr. Punk da Silva <70487766+mrpunkdasilva@users.noreply.github.com>
2024-12-25 16:39:23 -0300

    Update socialNetworks.jsx

--- src/data/socialNetworks.jsx
+++ src/data/socialNetworks.jsx
@@ -1,7 +1,7 @@
 import { FaGithub, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const listSocialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/dev-punk-da-silva" },
+  { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/gustavo-henrique-de-jesus" },
   { name: "github", icon: <FaGithub color={"white"} />, link: "https://www.github.com/mrpunkdasilva" },
   { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/punk.da.silva" },
   { name: "twitter", icon: <FaTwitter />, link: "https://www.x.com/MrPunkSama" }
