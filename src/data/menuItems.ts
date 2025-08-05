5f2f601f82ed30b1419c468a2df7380555c9cfd4
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-05 12:00:33 -0300

    refactor: Use centralized menuItems data in CyberNav

---
+++ src/data/menuItems.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+  {label: 'Home', href: '/'},
+  {label: 'About', href: '/#about'},
+  {label: 'Skills', href: '/#skills'},
+  {label: 'Experience', href: '/experience'},
+  {label: 'Projects', href: '/projects'},
+  {label: 'Contact', href: '/#contact'}
+];
