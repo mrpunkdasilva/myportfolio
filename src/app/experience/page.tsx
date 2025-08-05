a7b68f640ea7c5f491f10e523d7cdba7eb448d98
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-05 13:10:51 -0300

    change style

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,10 +1,4 @@
-import { Metadata } from 'next';
 import { ExperienceSection } from '@/components/ExperienceSection';
-
-export const metadata: Metadata = {
-  title: 'Gustavo Henrique | Experience',
-  description: "Explore Gustavo Henrique's professional experience, detailed using the STAR method.",
-};
 
 export default function ExperiencePage() {
   return (
