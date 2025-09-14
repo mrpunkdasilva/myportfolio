25f84f1ca1a8017f07f4ff1418343b0934ca1a75
Mr. Punk da Silva <70487766+mrpunkdasilva@users.noreply.github.com>
2025-09-13 23:02:01 -0300

    Update socialNetworks.tsx

--- src/data/socialNetworks.tsx
+++ src/data/socialNetworks.tsx
@@ -5,7 +5,7 @@ const listSocialNetworks = [
   { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/gustavo-henrique-de-jesus" },
   { name: "github", icon: <FaGithub color={"white"} />, link: "https://www.github.com/mrpunkdasilva" },
   { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/punk.da.silva" },
-  { name: "twitter", icon: <FaTwitter />, link: "https://www.x.com/MrPunkSama" },
+  // { name: "twitter", icon: <FaTwitter />, link: "https://www.x.com/MrPunkSama" },
   { name: "tabnews", icon: <FaNewspaper />, link: "https://www.tabnews.com.br/mrpunkdasilva" },
   { name: "dev.to", icon: <SiDevdotto />, link: "https://dev.to/mrpunkdasilva" }
 ];
