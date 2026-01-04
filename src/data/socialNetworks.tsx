b909ffabd0e6a98789a1f3d631ab21f662c97465
Mr Punk da Silva <gustavojs417@gmail.com>
2026-01-03 22:45:16 -0300

    chore: remove instagram from social networks

--- src/data/socialNetworks.tsx
+++ src/data/socialNetworks.tsx
@@ -4,7 +4,7 @@ import { SiDevdotto } from "react-icons/si";
 const listSocialNetworks = [
   { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/gustavo-henrique-de-jesus" },
   { name: "github", icon: <FaGithub color={"white"} />, link: "https://www.github.com/mrpunkdasilva" },
-  { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/punk.da.silva" },
+  // { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/punk.da.silva" },
   // { name: "twitter", icon: <FaTwitter />, link: "https://www.x.com/MrPunkSama" },
   { name: "tabnews", icon: <FaNewspaper />, link: "https://www.tabnews.com.br/mrpunkdasilva" },
   { name: "dev.to", icon: <SiDevdotto />, link: "https://dev.to/mrpunkdasilva" }
